85711c28df2e78f14e4ab8499e8bd977c1fe2376
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-06-25 00:14:13 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,14 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
-        Hello. world! Hello. world!
+        
+      Hello. world! Hello. world!
+
+      <br />
+
+      Hello. world! Hello. world!
+
+      
       </h1>
     </div>
   );
